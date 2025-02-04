3c755a1868c52751aa5829f3bf0aa29903022fa9
cruiseqe2 <cruiseqe2@gmail.com>
2025-02-04 21:54:37 +0000

    Initial Commit

---
+++ BackgammonScoreKeeper/BackgammonScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgammonScoreKeeperApp.swift
+//  BackgammonScoreKeeper
+//
+//  Created by Mark Oelbaum on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BackgammonScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
